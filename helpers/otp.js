5b91503afcdaefb8dca5a7f6b7f7334f20260b14
Anshad-KT <anshadkt78@gmail.com>
2024-01-01 15:19:28 +0530

    new system commit3

--- helpers/otp.js
+++ helpers/otp.js
@@ -1,10 +1,7 @@
 
 require('dotenv').config()
 
-const accountSid = process.env.AUTH_SID
-const authToken = process.env.AUTH_TOKEN
-const client = require('twilio')(accountSid,authToken)
-const MessagingResponce = require('twilio').twiml.MessagingResponse
+
 
 const nodemailer = require('nodemailer')
 
